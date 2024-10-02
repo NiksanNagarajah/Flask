219679659e48494823a60cc4661ec96dd2f938cc
nniksan <nniksan@nniksan.myguest.virtualbox.org>
2024-10-02 19:55:00 +0200

    Maj affichage

--- 3.SQLAlchemy/bookInfo/views.py
+++ 3.SQLAlchemy/bookInfo/views.py
@@ -3,8 +3,6 @@ from flask import render_template
 from .models import get_sample
 
 @app.route('/')
-# def home():
-#     return "<h1>Hello World !</h1>"
 def home():
     return render_template(
         'home.html', 
